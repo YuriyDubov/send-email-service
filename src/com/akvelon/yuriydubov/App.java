02f1f3563219c89a3a0a59da2f84c34a6ac2d0be
Yuriy Dubov <yurii-dubov@yandex.ru>
2019-07-17 12:52:01 +0300

    using properties for config instead of json

--- src/com/akvelon/yuriydubov/App.java
+++ src/com/akvelon/yuriydubov/App.java
@@ -4,9 +4,10 @@ import com.akvelon.yuriydubov.logging.ConsoleLogger;
 import com.akvelon.yuriydubov.messageSender.EmailMessageSender;
 import com.akvelon.yuriydubov.gameProvider.NbaGameProvider;
 
-import org.json.simple.*;
-import org.json.simple.parser.JSONParser;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
@@ -15,28 +16,31 @@ public class App {
         StringBuffer response = NbaGameProvider.getAllGames();
         System.out.println(response.toString());
 
-        JSONObject json = readConfig();
-        String login = (String) json.get("login");
-        String password = (String) json.get("password");
-        String recipient = (String) json.get("recipient");
-        String[] recipients = new String[1];
-        recipients[0] = recipient;
-        String subject = "Java send mail example";
-        String body = "Welcome to JavaMail!";
+        Properties property = new Properties();
 
-        EmailMessageSender.send(login, password, recipients, subject, body);
-
-        System.out.println("Goodbye from Main");
-    }
-
-    public static JSONObject readConfig() {
         try {
-            Object obj = new JSONParser().parse(new FileReader("../../../config.json"));
-            return (JSONObject) obj;
+            property.load(App.class.getClassLoader().getResourceAsStream("config.properties"));
+            String login = property.getProperty("login");
+            String password = property.getProperty("password");
+            String recipient = property.getProperty("recipient");
+
+            System.out.println(password);
+
+            String[] recipients = new String[1];
+            recipients[0] = recipient;
+            String subject = "Java send mail example";
+            String body = "Welcome to JavaMail!";
+
+            EmailMessageSender.send(login, password, recipients, subject, body);
+
+            System.out.println("Goodbye from Main");
         }
-        catch (Exception ex) {
+        catch (FileNotFoundException ex) {
+            System.err.println("File with properties not found");
             ex.printStackTrace();
         }
-        return new JSONObject();
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
