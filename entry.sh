b1d6e3c46221f4ca1cad6eff4f0503613c2d2b70
Yuriy Dubov <yuriy.dubov@inyar.ru>
2019-08-07 13:47:21 +0300

    add dockerfile for build and with cron. Cron is not working

---
+++ entry.sh
@@ -0,0 +1,4 @@
+ #!/bin/sh
+ # start cron
+ crond -f -l 8
+ # echo "This is a script, run bitch!"
---
+++ script.sh
@@ -0,0 +1,5 @@
+ #!/bin/sh
+ # code goes here.
+ echo "$USER"
+ java -jar /work/build/SendEmailService.jar
+ echo "This is a script, run by cron!"
